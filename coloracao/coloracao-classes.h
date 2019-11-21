dcc456f619bdb1a907d1d93a09ab4ace77b97ae3
ingridlagares <ingridlagares@hotmail.com>
2019-11-21 09:39:09 -0300

    Construtivas fix

--- coloracao/coloracao-classes.h
+++
@@ -1,8 +0,0 @@
-#ifndef CC_H
-#define CC_H
-
-void insert_color(Num *nodes, Vertex_colors *used_colors, int dim, int**graph, int color, int pos);
-void colorNodes(Num *nodes, Vertex_colors *used_colors, int dim, int**graph);
-int missing_color(Num *nodes, int dim);
-
-#endif
